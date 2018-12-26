f432f19000498cd3a5842e49d0e6e20c2b9259ee
unknown <alifnurrofli@gmail.com>
2018-12-26 11:01:56 +0700

    game

---
+++ Assets/GM.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GM : MonoBehaviour {
+
+    public static float coinTotal = 0;
+    public static float timeTotal = 0;
+
+    public static float zScenePos = 20;
+
+    public Transform NoPit;
+    public Transform RightPit;
+    public Transform LeftPit;
+    public Transform MidPit;
+
+    public Transform Coin;
+    public Transform Obstacle1;
+    public Transform Obstacle2;
+    public Transform Obstacle3;
+
+    public movtruck movtruckscript;
+
+    int randomNumber;
+
+	// Use this for initialization
+	void Start () {
+        //
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (zScenePos < 1200)
+        {
+            randomNumber = Random.Range(0, 12);
+
+            if (randomNumber < 4)
+            {
+                randomNumber = Random.Range(1, 7);
+
+                if (randomNumber == 1)
+                {
+                    Instantiate(NoPit, new Vector3(0, 0, zScenePos), NoPit.rotation);
+                    Instantiate(Coin, new Vector3(0, 1, zScenePos), Coin.rotation);
+                    zScenePos += 4;
+                }
+                if (randomNumber == 2)
+                {
+                    Instantiate(NoPit, new Vector3(0, 0, zScenePos), NoPit.rotation);
+                    Instantiate(Coin, new Vector3(1, 1, zScenePos), Coin.rotation);
+                    zScenePos += 4;
+                }
+                if (randomNumber == 3)
+                {
+                    Instantiate(NoPit, new Vector3(0, 0, zScenePos), NoPit.rotation);
+                    Instantiate(Coin, new Vector3(-1, 1, zScenePos), Coin.rotation);
+                    zScenePos += 4;
+                }
+                if (randomNumber == 4)
+                {
+                    Instantiate(RightPit, new Vector3(0, 0, zScenePos), RightPit.rotation);
+                    Instantiate(Coin, new Vector3(0, 1, zScenePos), Coin.rotation);
+                    zScenePos += 4;
+                    
+                }
+                if (randomNumber == 5)
+                {
+                    Instantiate(RightPit, new Vector3(0, 0, zScenePos), RightPit.rotation);
+                    Instantiate(Coin, new Vector3(-1, 1, zScenePos), Coin.rotation);
+                    zScenePos += 4;
+                }
+                if (randomNumber == 6)
+                {
+                    Instantiate(LeftPit, new Vector3(0, 0, zScenePos), LeftPit.rotation);
+                    Instantiate(Coin, new Vector3(0, 1, zScenePos), Coin.rotation);
+                    zScenePos += 4;
+                }
+
+                if (randomNumber == 7)
+                {
+                    Instantiate(NoPit, new Vector3(0, 0, zScenePos), NoPit.rotation);
+                    Instantiate(Coin, new Vector3(1, 1, zScenePos), Coin.rotation);
+                    zScenePos += 4;
+                }
+
+            }
+
+
+            if (randomNumber == 4)
+            {
+                Instantiate(RightPit, new Vector3(0, 0, zScenePos), RightPit.rotation);
+                Instantiate(Obstacle3, new Vector3(0, 1, zScenePos-2), Obstacle3.rotation);
+                zScenePos += 4;
+            }
+
+
+            if (randomNumber == 5)
+            {
+                Instantiate(RightPit, new Vector3(0, 0, zScenePos), RightPit.rotation);
+                Instantiate(Obstacle3, new Vector3(-1, 1, zScenePos), Obstacle3.rotation);
+                zScenePos += 4;
+            }
+
+            if (randomNumber == 6)
+            {
+                Instantiate(MidPit, new Vector3(0, 0, zScenePos), MidPit.rotation);
+                Instantiate(Obstacle2, new Vector3(1, 1, zScenePos-2), Obstacle2.rotation);
+                zScenePos += 4;
+            }
+
+
+            if (randomNumber == 7)
+            {
+                Instantiate(MidPit, new Vector3(0, 0, zScenePos), MidPit.rotation);
+                Instantiate(Obstacle2, new Vector3(-1, 1, zScenePos), Obstacle2.rotation);
+                zScenePos += 4;
+            }
+
+            if (randomNumber == 8)
+            {
+                Instantiate(LeftPit, new Vector3(0, 0, zScenePos), LeftPit.rotation);
+                Instantiate(Obstacle1, new Vector3(0, 1, zScenePos), Obstacle1.rotation);
+                zScenePos += 4;
+            }
+
+
+            if (randomNumber == 9)
+            {
+                Instantiate(LeftPit, new Vector3(0, 0, zScenePos), LeftPit.rotation);
+                Instantiate(Obstacle1, new Vector3(1, 1, zScenePos), Obstacle1.rotation);
+                zScenePos += 4;
+            }
+
+            if (randomNumber > 9)
+            {
+                Instantiate(NoPit, new Vector3(0, 0, zScenePos), NoPit.rotation);
+                zScenePos += 4;
+            }
+        }
+	}
+}
---
+++ Assets/movtruck.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movtruck : MonoBehaviour {
+
+    public Transform[] obstacle;
+    public int current = 0;
+
+    public float horizVel = 0;
+    public int laneNum = 1;
+
+    // Update is called once per frame
+    void Update () {
+        GetComponent<Rigidbody>().velocity = new Vector3(horizVel, 0, 4);
+
+        if (((obstacle[current].position.z - transform.position.z) <= 3) && (current <= 2))
+        {
+            Debug.Log("Jarak " + (transform.position.z - obstacle[current].position.z));
+            Debug.Log("Sekarang obs ke-" + current + ". Posisinya z -nya " + obstacle[current].position.z);
+            if (laneNum==1)
+            {
+                Debug.Log("Dari " + laneNum);
+                horizVel = 2;
+                StartCoroutine(stopSlide());
+                laneNum += 1;
+                current += 1;
+                Debug.Log("Ke " + laneNum);
+            }
+
+            else if (laneNum==2)
+            {
+                Debug.Log("Dari " + laneNum);
+                horizVel = 2;
+                StartCoroutine(stopSlide());
+                laneNum += 1;
+                current += 1;
+                Debug.Log("Ke " + laneNum);
+            }
+
+            else if (laneNum==3)
+            {
+                Debug.Log("Dari " + laneNum);
+                horizVel = -2;
+                StartCoroutine(stopSlide());
+                laneNum -= 1;
+                current += 1;
+                Debug.Log("Ke " + laneNum);
+            }
+        } else{}
+            
+	}
+
+    IEnumerator stopSlide()
+    {
+        yield return new WaitForSeconds(.5f);
+        horizVel = 0;
+    }
+}
